b8a2a4462850ad1cb68106dcf223b91bacf151c5
Aadityaa177 <aadijmurugan@gamil.cm>
2026-05-25 12:15:04 +0530

    Adding new column and row

--- day1.py
+++ day1.py
@@ -1,8 +1,20 @@
 import pandas as pd
 print(pd.__version__)
 
-calories = {"day1":1750, "day2":2100,"day3":1700}
-#no need index paramenter here since we have key value pairs
-ser = pd.Series(calories)
+#dataframe : A tabular data sttructure with rows and columns . (2 Dimensionals)
+# similar to an excel spreadsheet
 
-print(ser.loc["day2"])
+data = {"Name": ["Spongebob", "Patrick","Squidward"],
+        "Age": [30,35,50]
+        }
+
+df = pd.DataFrame(data, index=["Employee 1","Employee 2","Employee 3"])
+
+#Add a new column 
+df["Job"] = ["Cook","N/A", "Casier"]
+
+#Add a new row
+new_row = pd.DataFrame([{"Name": "Sandy", "Age": 28, "Job":"Doctor"}],index=["Employee 4"])
+df = pd.concat([df,new_row])
+
+print(df)
