c648e25f8e3ba3cd5d8945b156868c1c2e5a96c0
Aadityaa177 <aadijmurugan@gamil.cm>
2026-05-29 19:24:40 +0530

    Filtering based on logical operators

--- day1.py
+++ day1.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 df = pd.read_csv("pokemon.csv", index_col = "Name")
 
-pokemon = input("Enter a Pokemon Name: ")
+# Filtering : keeping the rows that match a condition
 
-try:
-    print(df.loc[pokemon])
-except KeyError:
-    print(f"{pokemon} not found")
+tall_pokemon = df[df["Height"]>=2]
 
+heavy_pokemon = df[df["Weight"] > 100]
+
+legendary = df[df["Legendary"]==1]
+
+water_poke = df[(df["Type1"]=="Water")|(df["Type2"] == "Water")]
+
+print(water_poke)
