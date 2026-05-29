f0984fe5f58653d318edf76a5e03ce63b921229d
Aadityaa177 <aadijmurugan@gamil.cm>
2026-05-29 19:29:25 +0530

    Getting the mean of numeric valued properties alone

--- day1.py
+++ day1.py
@@ -1,15 +1,8 @@
 import pandas as pd
 
+# aggregate - Reduces a set of values into a dingle summary value
+#             Used to summarize and analyse data
+#             Often used with the groupby() function
 df = pd.read_csv("pokemon.csv", index_col = "Name")
 
-# Filtering : keeping the rows that match a condition
-
-tall_pokemon = df[df["Height"]>=2]
-
-heavy_pokemon = df[df["Weight"] > 100]
-
-legendary = df[df["Legendary"]==1]
-
-water_poke = df[(df["Type1"]=="Water")|(df["Type2"] == "Water")]
-
-print(water_poke)
+print(df.mean(numeric_only=True))
