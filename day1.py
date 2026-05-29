e3c70d83e13487d72a35e62b9a79c3507b07f4e1
Aadityaa177 <aadijmurugan@gamil.cm>
2026-05-29 19:00:12 +0530

    Selection by ROWS

--- day1.py
+++ day1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("pokemon.csv")
+df = pd.read_csv("pokemon.csv", index_col = "No")
 
-# selection by column 
-#print(df["Name"].to_string())
-print(df["Height"].to_string())
+# selection by rows
+print(df.loc[25])
