3713e5d84ed5f52e1c27dad6e024f325adc92dbd
Aadityaa177 <aadijmurugan@gamil.cm>
2026-05-29 18:51:41 +0530

    importing csvfiles in pandas

--- day1.py
+++ day1.py
@@ -1,20 +1,6 @@
 import pandas as pd
-print(pd.__version__)
 
-#dataframe : A tabular data sttructure with rows and columns . (2 Dimensionals)
-# similar to an excel spreadsheet
+df = pd.read_csv("pokemon.csv")
 
-data = {"Name": ["Spongebob", "Patrick","Squidward"],
-        "Age": [30,35,50]
-        }
 
-df = pd.DataFrame(data, index=["Employee 1","Employee 2","Employee 3"])
-
-#Add a new column 
-df["Job"] = ["Cook","N/A", "Casier"]
-
-#Add a new row
-new_row = pd.DataFrame([{"Name": "Sandy", "Age": 28, "Job":"Doctor"}],index=["Employee 4"])
-df = pd.concat([df,new_row])
-
-print(df)
+print(df.to_string())
